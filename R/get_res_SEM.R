0e647cb52c64c5458c4533d1c189b9dd7a31cca7
ViktoriiaRadchuk <radchuk.victoria@gmail.com>
2019-03-09 21:02:35 +0100

    extract SEM results

---
+++ R/get_res_SEM.R
@@ -0,0 +1,67 @@
+#' Extracts results of the fitted structural equation model
+#'
+#' \code{get_res_SEM} extracts results of the fitted
+#'  structural equation model
+#'
+#' @param mod_obj fitted psem object, returned with function
+#' \code{\link{fit_mod}}
+#'
+#' @export
+#'
+#' @return A tibble with 12 columns that represent the metadata for a
+#' given study ID and the results of SEM: Cstat, dTable, R2 of all
+#' equations and path coefficients.
+#'
+#' @examples
+#' biol_dat <- read.csv('./data-raw/Closed_Data_26_02.csv')
+#' biol_stand <- convert_JulianDay(biol_data = biol_dat)
+#' # example for ID = 1
+#' subs <- droplevels(biol_stand[biol_stand$ID == 1, ])
+#' test_fSEM <- readRDS(paste0('./output_climwin/', subs$ID[1], '_',
+#'                     subs$Species[1], '_', subs$Location[1],
+#'                     '_', subs$Trait[1], '_ForSEM',  '.RDS'))
+#' dat <- test_fSEM$data_res[[1]]
+#' ## impute the DR
+#' dat <- impute_ma(data = dat, column = 'Demog_rate_mean')
+#' dat <- impute_median(data = dat, column = 'Demog_rate_SE')
+#' ## impute the Pop Size
+#' dat <- impute_ma(data = dat, column = 'Pop_mean')
+#' if(unique(subs$Count) == 'N'){
+#' dat <- impute_median(data = dat, column = 'Pop_SE')
+#' }
+#' ## calculate GR
+#' data_GR <- dat %>%
+#' dplyr::mutate(., Pop_mean_lag = lag(Pop_mean)) %>%
+#' dplyr::mutate(., GR = Pop_mean / Pop_mean_lag) %>%
+#' dplyr::filter(., !is.na(GR))
+#'
+#' # fit the model
+#' mod_SEM <- fit_mod(biol_data = data_GR, ID =1,
+#'                 DD = FALSE, weights = FALSE,
+#'                 correlation = FALSE)  ## does not work for corr = TRUE yet
+#' res_SEM <- get_res_SEM(mod_SEM)
+
+get_res_SEM <- function(mod_obj){
+
+  summary_mod <- summary(mod_obj, .progressBar = F)
+  dTable <- summary_mod$dTable
+  Cstat <- summary_mod$Cstat
+  R2_Relation <- piecewiseSEM::rsquared(mod_obj)
+  coefs <- piecewiseSEM::coefs(mod_obj)
+
+  data <- mod_obj$data
+  res <- tibble::tibble(ID = unique(data$ID),
+                        Species = unique(data$Species),
+                        Location = unique(data$Location),
+                        Trait_Categ = unique(data$Trait_Categ),
+                        Trait = unique(data$Trait),
+                        Demog_rate_Categ = unique(data$Demog_rate_Categ),
+                        Demog_rate = unique(data$Demog_rate),
+                        Count = unique(data$Count),
+                        dTable = list(dTable),
+                        Cstat = list(Cstat),
+                        R2 = list(R2_Relation),
+                        coefs = list(coefs))
+
+  return(res)
+}
