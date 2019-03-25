a886f695bd21eb8fe1049216882047d68121552d
ViktoriiaRadchuk <radchuk.victoria@gmail.com>
2019-03-25 12:02:35 +0100

    extract the results from SEM

---
+++ R/extract_res_SEM.R
@@ -0,0 +1,40 @@
+#' Extract results of the fitted SEM concerning a certain aspect of the model
+#'
+#' \code{extract_res_SEM} extracts the results of the list of models fitted with
+#' \code{\link{fit_SEM}} function. The results are extracted with a focus on a particular aspect
+#' of the model: Cstat value, R2 values or obtained path coefficients
+#'
+#' @param list_fitSEM List of SEMs fitted with the funciton \code{\link{fit_SEM}}.
+#' @param stat_extr Character specifying on which aspect of the fitted SEM
+#' you want to extract the results. Can take on the following values: 'coefs',
+#' 'R2', 'Cstat'.
+#'
+#' @export
+#'
+#' @return A dataframe containing the meta-data on each study (e.g. study is,
+#' species name, location, trait category, demographic rate category etc) and
+#' the data on the requested aspect of the fitted SEM.
+#'
+#' still add the example
+
+extract_res_SEM <- function(list_fitSEM, stat_extr){
+  res <- bind_rows(lapply(1:length(list_fitSEM), FUN = function(x){
+    elem <- list_fitSEM[[x]]
+    metadat <- elem[c('ID', 'Species', 'Location',
+                      'Trait_Categ', 'Trait', 'Demog_rate_Categ',
+                      'Demog_rate', 'Count', 'Nyears',
+                      'weights', 'DD', 'corr')]
+    test <- elem[[stat_extr]][[1]]
+
+    if(stat_extr == 'coefs'){
+      test <- test[, -ncol(test)]
+    }
+    test <- test %>% mutate_if(is.factor, as.character)
+    ret_dat <- cbind(test,
+                     metadat[rep(seq_len(nrow(metadat)), nrow(test)), ])
+    # to avoid coersion to character when binding the rows by bind_rows
+    ret_dat <- ret_dat %>% mutate_if(is.factor, as.character)
+  }
+  ))
+  return(res)
+}
