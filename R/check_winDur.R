098fcabcb93b0c28ff4cabef965ba13c59b25906
ViktoriiaRadchuk <radchuk.victoria@gmail.com>
2019-04-02 18:04:30 +0200

    deltaAIC threshold

--- R/check_winDur.R
+++ R/check_winDur.R
@@ -2,7 +2,8 @@
 #'
 #' \code{check_winDur} checks whether the window duration of the best
 #' selected model is within the specified range and if not, finds the
-#' model with sucha  window (given it is still better than random one)
+#' model with such a window (given that the model with such window
+#' is still better than the one without climatic variable as explanatory)
 #'
 #' @param biol_data Data frame with trait data for a given
 #' population and species.
@@ -12,6 +13,8 @@
 #' corresponding to the clim_data element as returned by \code{\link{climwin_proc}}.
 #' @param MinDur A numeric specifying the minimum allowed window duration.
 #' @param MaxDur A numeric specifying the maximum allowed window duration.
+#' @param deltaThresh A numeric specifying the minimum deltaAIC value for which the
+#' model will stil be judged acceptable for selecting the window duration.
 #'
 #' @export
 #'
@@ -35,22 +38,25 @@
 #' winDur <- check_winDur(climwin_out = test_rand$climwin_output[[1]],
 #'                        clim = test_rand$clim_data[[1]],
 #'                        biol_data = test_rand$biol_data[[1]],
-#'                        MinDur = 7, MaxDur = 300)
+#'                        MinDur = 7, MaxDur = 300, deltaThresh = -7)
 
 check_winDur <- function(climwin_out, clim,
-                         biol_data, MinDur, MaxDur){
+                         biol_data, MinDur,
+                         MaxDur, deltaThresh = -7){
   data_climwin <- climwin_out$Dataset
   data_climwin$WindowDur <- data_climwin$WindowOpen - data_climwin$WindowClose
 
   if(data_climwin$WindowDur[1] > MinDur & data_climwin$WindowDur[1] < MaxDur){
 
-    if(data_climwin$deltaAICc[1] <= -7){
+    if(data_climwin$deltaAICc[1] <= deltaThresh){
       message('The best selected model with slidingwin() returns the', '\n',
               'climatic window within the specified range', '\n',
               'Window duration is ', data_climwin$WindowDur[1], '\n',
               'and the deltaAICc is satisfactory: ', round(data_climwin$deltaAICc[1], 2),  '\n')
       dat_out <- cbind(biol_data, Clim =
-                         climwin_out$BestModelData[, c('climate')])
+                         climwin_out$BestModelData[, c('climate')],
+                       WinDur = data_climwin$WindowDur[1],
+                       deltaAIC = round(data_climwin$deltaAICc[1], 2))
       return(dat_out)
     }else{
       message('The best selected model with slidingwin() returns the', '\n',
@@ -68,7 +74,7 @@ check_winDur <- function(climwin_out, clim,
     while(data_climwin$WindowDur[i] < MinDur | data_climwin$WindowDur[i] > MaxDur){
       i <- i + 1
     }
-    if(data_climwin$deltaAICc[i] <= -7){
+    if(data_climwin$deltaAICc[i] <= deltaThresh){
       message('The identified climatic window within the specified range', '\n',
               'has the duration of ', data_climwin$WindowDur[i], '\n',
               'and the deltaAICc is satisfactory: ', round(data_climwin$deltaAICc[i], 2),  '\n')
@@ -85,7 +91,9 @@ check_winDur <- function(climwin_out, clim,
                                            type = 'absolute', refday = c(1, data_climwin$Reference.month[1]),
                                            cmissing = 'method2', cinterval = 'day')
       dat_out <- cbind(biol_data, Clim =
-                         single_sel_win$BestModelData[, c('climate')])
+                         single_sel_win$BestModelData[, c('climate')],
+                       WinDur = data_climwin$WindowDur[i],
+                       deltaAIC = round(data_climwin$deltaAICc[i], 2))
       return(dat_out)
     } else {
       message('The identified climatic window within the specified range', '\n',
