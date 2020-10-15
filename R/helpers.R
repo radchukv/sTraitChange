536be28ca51a9c5d70c88c61b8344bee8743435c
ViktoriiaRadchuk <radchuk.victoria@gmail.com>
2020-10-15 09:16:09 +0200

    extract and compute statistics for the effects in the fitted model + save the table with stats results

--- R/helpers.R
+++ R/helpers.R
@@ -93,3 +93,71 @@ extr_coefs <- function(obj, Type_EfS){
   sub <- obj$data_meta[[1]]$data_EfS[data$data_meta[[1]]$names == Type_EfS][[1]]
   return(sub)
 }
+
+
+#' Replace the stats in the table by the correct values from the omnibus test
+#'
+#' \code{replace_stats} replaces the values in the table by the correct output of
+#' omnibus test.
+#'
+#' @param data A dataframe containing the output of the model summary
+#' @param variable A character specifying the name of the variable for which the omnibus
+#' test is ot be computed.
+#'
+#' @return A dataframe that contains corrected statistics values for the specified variable.
+#' @export
+#'
+#' @examples
+#' Coefs_Aut <- readRDS(file = './output_overall_sig/PathCoefs_allMods_Temp_AlsoEstimatedRelations.RDS')
+#' traits <- read.csv('./data-raw/Species_traits_Subset_11_09.csv')
+#' Coef_Aut_IndDR <- droplevels(Coefs_Aut %>%
+#' dplyr::filter(., Relation == 'Ind_DemRate<-det_Clim'))
+#' Coefs_Aut_sp <- base::merge(Coef_Aut_IndDR, traits_proc, by = 'Species')
+#' mod_genLength <- metafor::rma.mv(Estimate ~ GenLength_y_IUCN + Trait_Categ + Demog_rate_Categ +
+#'                             GenLength_y_IUCN * Trait_Categ + GenLength_y_IUCN * Demog_rate_Categ +
+#'                             Pvalue,
+#'                             V = SError^2, random = list(~ 1|Species, ~1|ID, ~1|Location),
+#'                             data = Coefs_Aut_sp, method = 'ML')
+#' stats <- stats::coef(summary(mod_genLength))
+#' Inter_trait <- stats::anova(mod, btt = grep(':Trait', stats$Parameter))
+#' DR <- stats::anova(mod, btt = which(stats$Parameter %in% stats$Parameter[grepl('Demog_rate', stats$Parameter) & !grepl(':', stats$Parameter)]))
+#' stats_new <- replace_stats(data = stats, variable = ':Trait', stats_out = Inter_trait)
+#' stats_new1 <- replace_stats(data = stats, variable = 'Demog_rate', stats_out = DR)
+replace_stats <- function(data, variable, stats_out){
+  if(length(grep(':', variable)) > 0){
+    data$Chi2[grep(variable, data$Parameter)[1]] <- stats_out$QM
+    data$DF[grep(variable, data$Parameter)[1]] <- stats_out$m
+    data$pval[grep(variable, data$Parameter)[1]] <- stats_out$QMp
+    if(length(grep(variable, data$Parameter)) > 1){
+      for(i in seq(from = 2, to = length(grep(variable, data$Parameter)))){
+        data$Chi2[grep(variable, data$Parameter)[i]] <- NA
+        data$DF[grep(variable, data$Parameter)[i]] <- NA
+        data$pval[grep(variable, data$Parameter)[i]] <- NA
+      }
+    }
+    return(data)
+  } else {
+    data$Chi2[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                         !grepl(':', data$Parameter)])[1]] <- stats_out$QM
+    data$pval[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                         !grepl(':', data$Parameter)])[1]] <- stats_out$QMp
+    data$DF[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                       !grepl(':', data$Parameter)])[1]] <- stats_out$m
+
+    if(length(which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                       !grepl(':', data$Parameter)])) > 1){
+      for(i in seq(from = 2,
+                   to = length(which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                                        !grepl(':', data$Parameter)])))){
+        data$Chi2[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                             !grepl(':', data$Parameter)])[i]] <- NA
+        data$DF[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                           !grepl(':', data$Parameter)])[i]] <- NA
+        data$pval[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
+                                                             !grepl(':', data$Parameter)])[i]] <- NA
+        return(data)
+      }
+    }
+  }
+}
+
