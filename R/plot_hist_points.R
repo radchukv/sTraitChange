838484340354d7ccd5cd8d004f2d4953aedbbc01
radchukv <radchuk.victoria@gmail.com>
2021-02-24 11:38:17 +0100

    fix: possibility to set TraitDem to NULL when the analyses are only split by trait category (e.g. for SimpleSEM)

--- R/plot_hist_points.R
+++ R/plot_hist_points.R
@@ -12,8 +12,9 @@
 #' \code{\link{fit_all_meta}}.
 #' @param dataAxis A data frame specifying the names of the relations to be plotted on the
 #' y axis.
-#' @param Traitdem Character specifying the level of the trait, for which analyses were
-#' conducted.
+#' @param Traitdem Character specifying the combination of the trait and demographic rate
+#' categories, for which analyses were conducted. In case analyses were conducted separately
+#' for trait category only, set to NULL.
 #' @param tabC A data frame containing the number of studies per each combination of
 #' trait and demographic rate category.
 #' @param angle_yax_lab Numeric specifying whether the labels of relations provided with
@@ -25,6 +26,7 @@
 #' the plot combines the relations from simple and complex SEMs, otherwise set to NULL
 #' (default).
 #'
+#' @inheritParams fit_all_meta
 #' @export
 #'
 #' @return Plots a forest-like plot with, in addition to the overall effect size estimates
@@ -39,8 +41,10 @@ plot_hist_points <- function(data_allEstim,
                            dataAxis,
                            Traitdem = "Morphology + Recruitment",
                            tabC,
-                           angle_yax_lab = 90, annot = NULL){
+                           angle_yax_lab = 90, annot = NULL,
+                           Trait_categ= NULL){
   if(is.null(annot)){
+    if(! is.null(Traitdem)){
     ggplot() + geom_histogram(data = subset(data_allEstim, TraitDem == Traitdem),
                               aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
       scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
@@ -81,6 +85,48 @@ plot_hist_points <- function(data_allEstim,
       labs(x = 'Effect size', y = '') + ggtitle(Traitdem) +
       geom_text(data = subset(tabC, TraitDem == Traitdem),
                 aes(x = x, y = y, label = Count), col = c('red4', 'royalblue1'))
+    } else {
+      ggplot() + geom_histogram(data = subset(data_allEstim, Trait_Categ == Trait_categ),
+                                aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
+        scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
+                                     'Precipitation' = 'lightblue1')) +
+        geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
+        theme_bw() +
+        theme(strip.background = element_blank(),
+              strip.text = element_blank(),
+              strip.placement = "outside",
+              panel.spacing = unit(0, "lines"),
+              legend.position = 'bottom',
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.text.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              plot.margin = margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
+              legend.direction = 'vertical',
+              panel.border = element_rect(fill = NA, color = 'grey',
+                                          size = 0.1, linetype = 3))  +  ## , panel.border = element_blank()
+        geom_errorbar(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
+                      width=.1,
+                      aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col, y = 5), lwd = 0.5) +
+        geom_point(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
+                   aes(y = 5, x = Estimate, shape = Climatic_var, color = Col), size = 1.5) +
+        facet_grid(rows = vars(REL_Clim)) +
+        scale_colour_manual(values = c("Signif, temperature" = "red4",
+                                       "Non-signif, temperature" = "tomato1",
+                                       "Signif, precipitation" = "royalblue1",
+                                       "Non-signif, precipitation" = "deepskyblue2")) +
+        scale_shape_manual(values = c(16, 15)) +
+        guides(color = guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
+               fill = guide_legend(ncol= 1, title = 'All effect sizes',
+                                   override.aes = list(shape = c(NA, NA))),
+               shape = guide_legend(title = 'Climatic variable')) +
+        coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
+        geom_text(data = dataAxis, aes(x = x, y = y, label = label), angle= angle_yax_lab,
+                  vjust = 0.5, hjust = 0.6) +
+        labs(x = 'Effect size', y = '') + ggtitle(Trait_categ) +
+        geom_text(data = subset(tabC, Trait_Categ == Trait_categ),
+                  aes(x = x, y = y, label = Count), col = c('red4', 'royalblue1'))
+    }
   } else {
     ggplot() + geom_blank(data = subset(data_allEstim, TraitDem == Traitdem),
                           aes(x = Estimate)) +
