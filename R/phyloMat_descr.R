b006f6b471ac290b08bde689b7eeb1458ff6c961
radchukv <radchuk.victoria@gmail.com>
2024-10-29 14:35:01 +0100

    update description of the phylogen matrix

--- R/phyloMat_descr.R
+++ R/phyloMat_descr.R
@@ -1,18 +1,7 @@
 #' @title phyloMat
 #' @description Matrix of phylogenetic correlation between the
 #' species for which phenological traits were measured
-#' @format A matrix with 5 rows and 5 columns that contain
+#' @format A matrix with 58 rows and 58 columns that contain
 #' phylogenetic correlations between species
-#' \describe{
-#'   \item{\code{Urocitellus_columbianus}}{numeric Phylogenetic correlations
-#'   of Urocitellus columbianus with other species}
-#'   \item{\code{Parus_major}}{numeric Phylogenetic correlations
-#'   of Parus major with other species}
-#'   \item{\code{Athene_noctua}}{numeric Phylogenetic correlations
-#'   of Athene noctua with other species}
-#'   \item{\code{Otus_scops}}{numeric Phylogenetic correlations
-#'   of Otus scops with other species}
-#'   \item{\code{Zootoca_vivipara}}{numeric Phylogenetic correlations
-#'   of Zootoca vivipara with other species}
-#'}
+#'
 "phyloMat"
