b7d189d4dabe5ead2331d190aa341dfdad3e859c
radchukv <radchuk.victoria@gmail.com>
2024-10-30 14:17:33 +0100

    fix: namespace for plot_hist_points()

--- R/plot_hist_points.R
+++ R/plot_hist_points.R
@@ -34,147 +34,162 @@
 #' effect sizes. Each panel corresponds to a single relation (see \code{Type_EfS} in
 #' \code{\link{fit_meta}} function) and effect sizes for precipitation and temperature
 #' are shown in different colors and shapes.
-#' ## Still add examples
 #'
 plot_hist_points <- function(data_allEstim,
                            Ef_sizesEstim,
                            dataAxis,
-                           Traitdem = "Morphology + Recruitment",
+                           Traitdem = "Phenology",
                            tabC,
                            angle_yax_lab = 90, annot = NULL,
-                           Trait_categ= NULL){
+                           Trait_categ = NULL){
   if(is.null(annot)){
     if(! is.null(Traitdem)){
-    ggplot() + geom_histogram(data = subset(data_allEstim, TraitDem == Traitdem),
-                              aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
-      scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
+    ggplot2::ggplot() + ggplot2::geom_histogram(data = subset(data_allEstim, TraitDem == Traitdem),
+                                                ggplot2::aes(x = Estimate, fill = Climatic_var),
+                                                alpha = 0.5, bins = 50) +
+        ggplot2::scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
                                    'Precipitation' = 'lightblue1')) +
-      geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
-      theme_bw() +
-      theme(strip.background = element_blank(),
-            strip.text = element_blank(),
+        ggplot2::geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
+        ggplot2::theme_bw() +
+        ggplot2::theme(strip.background = ggplot2::element_blank(),
+            strip.text = ggplot2::element_blank(),
             strip.placement = "outside",
-            panel.spacing = unit(0, "lines"),
+            panel.spacing = ggplot2::unit(0, "lines"),
             legend.position = 'bottom',
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            plot.margin = margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
+            panel.grid.major = ggplot2::element_blank(),
+            panel.grid.minor = ggplot2::element_blank(),
+            axis.text.y = ggplot2::element_blank(),
+            axis.ticks.y = ggplot2::element_blank(),
+            plot.margin = ggplot2::margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
             legend.direction = 'vertical',
-            panel.border = element_rect(fill = NA, color = 'grey',
+            panel.border = ggplot2::element_rect(fill = NA, color = 'grey',
                                         size = 0.1, linetype = 3))  +  ## , panel.border = element_blank()
-      geom_errorbar(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
+        ggplot2::geom_errorbar(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
                     width=.1,
-                    aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col, y = 5), lwd = 0.5) +
-      geom_point(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
-                 aes(y = 5, x = Estimate, shape = Climatic_var, color = Col), size = 1.5) +
-      facet_grid(rows = vars(REL_Clim)) +
-      scale_colour_manual(values = c("Signif, temperature" = "red4",
+                    ggplot2::aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col,
+                                 y = 5), lwd = 0.5) +
+        ggplot2::geom_point(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
+                            ggplot2::aes(y = 5, x = Estimate, shape = Climatic_var,
+                                         color = Col), size = 1.5) +
+        ggplot2::facet_grid(rows = ggplot2::vars(REL_Clim)) +
+        ggplot2::scale_colour_manual(values = c("Signif, temperature" = "red4",
                                      "Non-signif, temperature" = "tomato1",
                                      "Signif, precipitation" = "royalblue1",
                                      "Non-signif, precipitation" = "deepskyblue2")) +
-      scale_shape_manual(values = c(16, 15)) +
-      guides(color = guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
-             fill = guide_legend(ncol= 1, title = 'All effect sizes',
+        ggplot2::scale_shape_manual(values = c(16, 15)) +
+        ggplot2::guides(color = ggplot2::guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
+             fill = ggplot2::guide_legend(ncol= 1, title = 'All effect sizes',
                                  override.aes = list(shape = c(NA, NA))),
-             shape = guide_legend(title = 'Climatic variable')) +
-      coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
-      geom_text(data = dataAxis, aes(x = x, y = y, label = label), angle= angle_yax_lab,
-                vjust = 0.5, hjust = 0.6) +
-      labs(x = 'Effect size', y = '') + ggtitle(Traitdem) +
-      geom_text(data = subset(tabC, TraitDem == Traitdem),
-                aes(x = x, y = y, label = Count), col = c('red4', 'royalblue1'))
+             shape = ggplot2::guide_legend(title = 'Climatic variable')) +
+        ggplot2::coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
+        ggplot2::geom_text(data = dataAxis, ggplot2::aes(x = x, y = y, label = label),
+                           angle= angle_yax_lab, vjust = 0.5, hjust = 0.6) +
+        ggplot2::labs(x = 'Effect size', y = '') + ggplot2::ggtitle(Traitdem) +
+        ggplot2::geom_text(data = subset(tabC, TraitDem == Traitdem),
+                           ggplot2::aes(x = x, y = y, label = Count),
+                           col = c('red4', 'royalblue1'))
     } else {
-      ggplot() + geom_histogram(data = subset(data_allEstim, Trait_Categ == Trait_categ),
-                                aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
-        scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
+      ggplot2::ggplot() + ggplot2::geom_histogram(data = subset(data_allEstim,
+                                                                Trait_Categ == Trait_categ),
+                                                  ggplot2::aes(x = Estimate, fill = Climatic_var),
+                                                  alpha = 0.5, bins = 50) +
+        ggplot2::scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
                                      'Precipitation' = 'lightblue1')) +
-        geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
-        theme_bw() +
-        theme(strip.background = element_blank(),
-              strip.text = element_blank(),
+        ggplot2::geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
+        ggplot2::theme_bw() +
+        ggplot2::theme(strip.background = ggplot2::element_blank(),
+              strip.text = ggplot2::element_blank(),
               strip.placement = "outside",
-              panel.spacing = unit(0, "lines"),
+              panel.spacing = ggplot2::unit(0, "lines"),
               legend.position = 'bottom',
-              panel.grid.major = element_blank(),
-              panel.grid.minor = element_blank(),
-              axis.text.y = element_blank(),
-              axis.ticks.y = element_blank(),
-              plot.margin = margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
+              panel.grid.major = ggplot2::element_blank(),
+              panel.grid.minor = ggplot2::element_blank(),
+              axis.text.y = ggplot2::element_blank(),
+              axis.ticks.y = ggplot2::element_blank(),
+              plot.margin = ggplot2::margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
               legend.direction = 'vertical',
-              panel.border = element_rect(fill = NA, color = 'grey',
+              panel.border = ggplot2::element_rect(fill = NA, color = 'grey',
                                           size = 0.1, linetype = 3))  +  ## , panel.border = element_blank()
-        geom_errorbar(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
+        ggplot2::geom_errorbar(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
                       width=.1,
-                      aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col, y = 5), lwd = 0.5) +
-        geom_point(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
-                   aes(y = 5, x = Estimate, shape = Climatic_var, color = Col), size = 1.5) +
-        facet_grid(rows = vars(REL_Clim)) +
-        scale_colour_manual(values = c("Signif, temperature" = "red4",
+                      ggplot2::aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col, y = 5), lwd = 0.5) +
+        ggplot2::geom_point(data = subset(Ef_sizesEstim, Trait_Categ == Trait_categ),
+                   ggplot2::aes(y = 5, x = Estimate, shape = Climatic_var, color = Col), size = 1.5) +
+        ggplot2::facet_grid(rows = ggplot2::vars(REL_Clim)) +
+        ggplot2::scale_colour_manual(values = c("Signif, temperature" = "red4",
                                        "Non-signif, temperature" = "tomato1",
                                        "Signif, precipitation" = "royalblue1",
                                        "Non-signif, precipitation" = "deepskyblue2")) +
-        scale_shape_manual(values = c(16, 15)) +
-        guides(color = guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
-               fill = guide_legend(ncol= 1, title = 'All effect sizes',
+        ggplot2::scale_shape_manual(values = c(16, 15)) +
+        ggplot2::guides(color = ggplot2::guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
+               fill = ggplot2::guide_legend(ncol= 1, title = 'All effect sizes',
                                    override.aes = list(shape = c(NA, NA))),
-               shape = guide_legend(title = 'Climatic variable')) +
-        coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
-        geom_text(data = dataAxis, aes(x = x, y = y, label = label), angle= angle_yax_lab,
-                  vjust = 0.5, hjust = 0.6) +
-        labs(x = 'Effect size', y = '') + ggtitle(Trait_categ) +
-        geom_text(data = subset(tabC, Trait_Categ == Trait_categ),
-                  aes(x = x, y = y, label = Count), col = c('red4', 'royalblue1'))
+               shape = ggplot2::guide_legend(title = 'Climatic variable')) +
+        ggplot2::coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
+        ggplot2::geom_text(data = dataAxis,
+                           ggplot2::aes(x = x, y = y, label = label),
+                           angle= angle_yax_lab, vjust = 0.5, hjust = 0.6) +
+        ggplot2::labs(x = 'Effect size', y = '') + ggplot2::ggtitle(Trait_categ) +
+        ggplot2::geom_text(data = subset(tabC, Trait_Categ == Trait_categ),
+                           ggplot2::aes(x = x, y = y, label = Count),
+                           col = c('red4', 'royalblue1'))
     }
   } else {
-    ggplot() + geom_blank(data = subset(data_allEstim, TraitDem == Traitdem),
-                          aes(x = Estimate)) +
-      geom_rect(data = annot, aes(fill = hue),
-                xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
-      scale_fill_manual(values = c('Complex SEM' = 'lightgrey',
+    ggplot2::ggplot() + ggplot2::geom_blank(data =
+                                              subset(data_allEstim, TraitDem == Traitdem),
+                                            ggplot2::aes(x = Estimate)) +
+      ggplot2::geom_rect(data = annot,
+                         ggplot2::aes(fill = hue), xmin = -Inf, xmax = Inf,
+                         ymin = -Inf, ymax = Inf) +
+      ggplot2::scale_fill_manual(values = c('Complex SEM' = 'lightgrey',
                                    'Simple SEM' = 'white'),
-                        guide =  guide_legend(title = 'Model')) +
+                        guide =  ggplot2::guide_legend(title = 'Model')) +
       ggnewscale::new_scale_fill() +
-      geom_histogram(data = subset(data_allEstim, TraitDem == Traitdem),
-                     aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
-      scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
+      ggplot2::geom_histogram(data = subset(data_allEstim, TraitDem == Traitdem),
+                     ggplot2::aes(x = Estimate, fill = Climatic_var), alpha = 0.5, bins = 50) +
+      ggplot2::scale_fill_manual(values = c('Temperature' = 'lightsalmon1',
                                    'Precipitation' = 'lightblue1')) +
-      geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
-      theme_bw() +
-      theme(strip.background = element_blank(),
-            strip.text = element_blank(),
+      ggplot2::geom_vline(xintercept = 0, linetype = 'dashed', color = 'black', lwd = 0.9) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(strip.background = ggplot2::element_blank(),
+            strip.text = ggplot2::element_blank(),
             strip.placement = "outside",
-            panel.spacing = unit(0, "lines"),
+            panel.spacing = ggplot2::unit(0, "lines"),
             legend.position = 'bottom',
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            axis.text.y = element_blank(),
-            axis.ticks.y = element_blank(),
-            plot.margin = margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
+            panel.grid.major = ggplot2::element_blank(),
+            panel.grid.minor = ggplot2::element_blank(),
+            axis.text.y = ggplot2::element_blank(),
+            axis.ticks.y = ggplot2::element_blank(),
+            plot.margin = ggplot2::margin(c(0.2, 0.2, 1.5, 1.5), unit = 'line'),
             legend.direction = 'vertical',
-            panel.border = element_rect(fill = NA, color = 'grey',
+            panel.border = ggplot2::element_rect(fill = NA, color = 'grey',
                                         size = 0.1, linetype = 3))  +  ## , panel.border = element_blank()
-      geom_errorbar(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
+      ggplot2::geom_errorbar(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
                     width=.1,
-                    aes(xmin = EfS_Low, xmax = EfS_Upper, colour = Col, y = 5), lwd = 0.5) +
-      geom_point(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
-                 aes(y = 5, x = Estimate, shape = Climatic_var, color = Col), size = 1.5) +
-      facet_grid(rows = vars(REL_Clim)) +
-      scale_colour_manual(values = c("Signif, temperature" = "red4",
+                    ggplot2::aes(xmin = EfS_Low, xmax = EfS_Upper,
+                                 colour = Col, y = 5), lwd = 0.5) +
+      ggplot2::geom_point(data = subset(Ef_sizesEstim, TraitDem == Traitdem),
+                          ggplot2::aes(y = 5, x = Estimate, shape = Climatic_var,
+                                       color = Col), size = 1.5) +
+      ggplot2::facet_grid(rows = ggplot2::vars(REL_Clim)) +
+      ggplot2::scale_colour_manual(values = c("Signif, temperature" = "red4",
                                      "Non-signif, temperature" = "tomato1",
                                      "Signif, precipitation" = "royalblue1",
                                      "Non-signif, precipitation" = "deepskyblue2")) +
-      scale_shape_manual(values = c(16, 15)) +
-      guides(color = guide_legend(ncol= 2, title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
-             fill = guide_legend(ncol= 1, title = 'All effect sizes',
+      ggplot2::scale_shape_manual(values = c(16, 15)) +
+      ggplot2::guides(color = ggplot2::guide_legend(ncol= 2,
+                                                    title = 'Overall effect size'),  ## ncol = 1 for ppt, where the legend is better on the right
+             fill = ggplot2::guide_legend(ncol= 1, title = 'All effect sizes',
                                  override.aes = list(shape = c(NA, NA))),
-             shape = guide_legend(title = 'Climatic variable')) +
-      coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
-      geom_text(data = dataAxis, aes(x = x, y = y, label = label), angle= angle_yax_lab,
+             shape = ggplot2::guide_legend(title = 'Climatic variable')) +
+      ggplot2::coord_cartesian(clip = 'off', xlim = c(-2, 2)) +
+      ggplot2::geom_text(data = dataAxis,
+                         ggplot2::aes(x = x, y = y, label = label),
+                         angle= angle_yax_lab,
                 vjust = 0.5, hjust = 0.6) +
-      labs(x = 'Effect size', y = '') + ggtitle(Traitdem) +
-      geom_text(data = subset(tabC, TraitDem == Traitdem),
-                aes(x = x, y = y, label = Count), col = c('red4', 'royalblue1'))
+      ggplot2::labs(x = 'Effect size', y = '') + ggplot2::ggtitle(Traitdem) +
+      ggplot2::geom_text(data = subset(tabC, TraitDem == Traitdem),
+                         ggplot2::aes(x = x, y = y, label = Count),
+                         col = c('red4', 'royalblue1'))
   }
 }
