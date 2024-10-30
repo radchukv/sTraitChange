5d988bd8420127c9bc36ea1b1ef58aa295b719f8
radchukv <radchuk.victoria@gmail.com>
2024-10-30 22:19:04 +0100

    delete unused helpers

--- R/helpers.R
+++ R/helpers.R
@@ -1,68 +1,3 @@
-#' Impute average of the values before and after the missing one
-#'
-#' \code{imput_ma} imputes the mean of the values before and after
-#' the missing value in the indicated column.
-#'
-#' @param data A dataframe containing the column for which the values have to be imputed.
-#' @param Column A character specifying the name of the column for which the missing values
-#' have to be imputed.
-#'
-#' @return A dataframe that contains imputed values for the column that had missing values
-#' initially, or the original dataframe if the indicated column did not contain missing
-#' values.
-#' @export
-#'
-#' @examples
-#' dat_birds <- read.csv('./data-raw/Test_european_birds.csv')
-#' sub_1 <- droplevels(subset(dat_birds, ID == 1))
-#' test <- impute_ma(data = sub_1, column = 'Trait_mean')
-#' sub_21 <- droplevels(subset(dat_birds, ID == 21))
-#' test21 <- impute_ma(data = sub_21, column = 'Trait_mean')
-impute_ma <- function(data, column){
-
-  onlycol <- data[column]
-  if (any(is.na(onlycol))) {
-    forMean <- cbind(onlycol[[column]][which(is.na(onlycol)) - 1],
-                     onlycol[[column]][which(is.na(onlycol)) + 1])
-
-    data[[column]][is.na(onlycol)] <- rowMeans(forMean)
-    return(data)
-  } else {
-    return(data)
-  }
-}
-
-
-#' Impute a median of all the values in the column
-#'
-#' \code{imput_median} imputes the median of all the values in the indicated column.
-#'
-#' @param data A dataframe containing the column for which the values have to be imputed.
-#' @param Column A character specifying the name of the column for which the missing values
-#' have to be imputed.
-#'
-#' @return A dataframe that contains imputed values for the column that had missing values
-#' initially, or the original dataframe if the indicated column did not contain missing
-#' values.
-#' @export
-#'
-#' @examples
-#' dat_birds <- read.csv('./data-raw/Test_european_birds.csv')
-#' sub_1 <- droplevels(subset(dat_birds, ID == 1))
-#' test <- impute_median(data = sub_1, column = 'Trait_SE')
-#' sub_21 <- droplevels(subset(dat_birds, ID == 21))
-#' test21 <- impute_median(data = sub_21, column = 'Trait_SE')
-impute_median <- function(data, column){
-  onlycol <- data[column]
-
-  if (any(is.na(onlycol))) {
-    data[[column]][is.na(onlycol)] <- median(data[[column]], na.rm = T)
-    return(data)
-  } else {
-    return(data)
-  }
-}
-
 #' Extract path coefficients from the output of \code{\link{fit_all_meta}} function
 #'
 #' \code{extr_coefs} extracts the path coefficients for a specified relation.
@@ -128,22 +63,6 @@ extr_coefs <- function(obj, Type_EfS){
 #' @return A dataframe that contains corrected statistics values for the specified variable.
 #' @export
 #'
-#' @examples
-#' Coefs_Aut <- readRDS(file = './output_overall_sig/PathCoefs_allMods_Temp_AlsoEstimatedRelations.RDS')
-#' traits <- read.csv('./data-raw/Species_traits_Subset_11_09.csv')
-#' Coef_Aut_IndDR <- droplevels(Coefs_Aut %>%
-#' dplyr::filter(., Relation == 'Ind_DemRate<-det_Clim'))
-#' Coefs_Aut_sp <- base::merge(Coef_Aut_IndDR, traits_proc, by = 'Species')
-#' mod_genLength <- metafor::rma.mv(Estimate ~ GenLength_y_IUCN + Trait_Categ + Demog_rate_Categ +
-#'                             GenLength_y_IUCN * Trait_Categ + GenLength_y_IUCN * Demog_rate_Categ +
-#'                             Pvalue,
-#'                             V = SError^2, random = list(~ 1|Species, ~1|ID, ~1|Location),
-#'                             data = Coefs_Aut_sp, method = 'ML')
-#' stats <- stats::coef(summary(mod_genLength))
-#' Inter_trait <- stats::anova(mod, btt = grep(':Trait', stats$Parameter))
-#' DR <- stats::anova(mod, btt = which(stats$Parameter %in% stats$Parameter[grepl('Demog_rate', stats$Parameter) & !grepl(':', stats$Parameter)]))
-#' stats_new <- replace_stats(data = stats, variable = ':Trait', stats_out = Inter_trait)
-#' stats_new1 <- replace_stats(data = stats, variable = 'Demog_rate', stats_out = DR)
 replace_stats <- function(data, variable, stats_out){
   if(length(grep(':', variable)) > 0){
     data$Chi2[grep(variable, data$Parameter)[1]] <- stats_out$QM
@@ -164,27 +83,6 @@ replace_stats <- function(data, variable, stats_out){
                                                          !grepl(':', data$Parameter)])] <- stats_out$QMp
     data$DF[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
                                                        !grepl(':', data$Parameter)])] <- stats_out$m
-    ## I do not understand why this was needed:
-    # %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                       !grepl(':', data$Parameter)])[1]]
-    # that idnex of [1] effectively adds the stats onlu to the first level of the categorical variable...
-    # maybe it has to do with the diff. ways of design matrix, check later
-    # for now I leave uncommented here
-
-    # if(length(which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                                                    !grepl(':', data$Parameter)])) > 1){
-    #   for(i in seq(from = 2,
-    #                to = length(which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                                                                     !grepl(':', data$Parameter)])))){
-    #     data$Chi2[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                                                          !grepl(':', data$Parameter)])[i]] <- NA
-    #     data$DF[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                                                        !grepl(':', data$Parameter)])[i]] <- NA
-    #     data$pval[which(data$Parameter %in% data$Parameter[grepl(variable, data$Parameter) &
-    #                                                          !grepl(':', data$Parameter)])[i]] <- NA
-    #     return(data)
-    #   }
-    # }
     return(data)
   }
 }
