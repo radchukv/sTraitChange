79aa6f59ebb6ff93bb5b7b8ad216d8adb29e4635
radchukv <radchuk.victoria@gmail.com>
2025-04-24 18:38:50 +0200

    fix grey colour by default

--- R/plot_concept.R
+++ R/plot_concept.R
@@ -169,8 +169,8 @@ plot_concept <- function(Trait_categ = 'Phenological',
            y =  c(miny, maxy)) +
       ggplot2::geom_blank() +
       ggplot2::geom_abline(data = ES_dat,
-                           ggplot2::aes(intercept = 0, slope = .data[[slope_ES]]),
-                  col = 'grey') +
+                           ggplot2::aes(intercept = 0, slope = .data[[slope_ES]])) +
+                  # col = 'grey') +
       ggplot2::geom_ribbon(data = dat_rib,
                            ggplot2::aes(x = x, ymin= ymin, ymax = ymax),
                   fill = 'black',
